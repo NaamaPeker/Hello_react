3ab2c20a2acd30d0042585181fa0b443fcc8f644
נעמה פקר <naamap66@gmail.com>
2025-06-19 16:14:54 +0300

    added users

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import GreetingCard from './GreetingCard';
 import './App.css';
+import UserList
+ from './UserList';
 
-function App() {
+ const users = ['naama', 'noam', 'hila', 'yali']
+
+ function App() {
   return (
     <div className="App">
       <header className="App-header">
        {
         <div>
           <GreetingCard name = "naama" messege = "good afternoon"></GreetingCard>
+        
+        <h1>All users</h1>
+        <UserList users = {users}/>
         </div>
-      }
+}
       </header>
     </div>
   );
